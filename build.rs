ed9a175f221a76dbff6933cdf1bd8bf4900dd7d4
Artem <artemciy@gmail.com>
2016-12-05 13:33:14 +0300

    Giving cargo the library path (Debian).

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-fn main() {
+fn main() {  // cf. http://doc.crates.io/build-script.html
   // libpq-dev libpq5
-  println! ("cargo:rustc-link-lib=pq");
-}
+  // println! ("cargo:rustc-link-lib=pq");
+  println! ("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");}
